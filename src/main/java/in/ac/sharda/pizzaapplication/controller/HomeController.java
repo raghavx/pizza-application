6decc41b48e7cbf9c817651ee2fdba2edf323f1a
Raghvendra <ragh@structure101.com>
2019-06-13 12:09:48 +0530

    web cart controller

--- src/main/java/in/ac/sharda/pizzaapplication/controller/HomeController.java
+++ src/main/java/in/ac/sharda/pizzaapplication/controller/HomeController.java
@@ -3,6 +3,7 @@ package in.ac.sharda.pizzaapplication.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import in.ac.sharda.pizzaapplication.data.Products;
 
@@ -10,9 +11,17 @@ import in.ac.sharda.pizzaapplication.data.Products;
 public class HomeController {
 
 	@GetMapping("/")
-	public String home(Model model){
-		model.addAttribute("products", Products
-				.getInstance().getProducts().values());
+	public String home(Model model) {
+		model.addAttribute("products", Products.getInstance().getProducts().values());
 		return "home";
+	}
+
+	@GetMapping("/web/product/detail/{id}")
+	public String detail(@PathVariable("id") int id, 
+			Model model) {
+		model.addAttribute("product", 
+				Products.getInstance()
+				.getProducts().get(id));
+		return "productdetail";
 	}
 }
---
+++ src/main/java/in/ac/sharda/pizzaapplication/controller/WebCartController.java
@@ -0,0 +1,8 @@
+package in.ac.sharda.pizzaapplication.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebCartController {
+
+}
